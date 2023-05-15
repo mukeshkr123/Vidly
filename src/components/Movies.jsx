700e935c4757671c68df1f7dc386e971ed6a80f4
mukesh <mukeshmehta2041@gmail.com>
2023-05-15 15:23:20 +0530

    filtering is done through genres

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -36,27 +36,39 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    console.log(genre);
+    this.setState({ selectedGenre: genre });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allmovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allmovies,
+    } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const movies = Paginate(allmovies, currentPage, pageSize);
+    const filtered = selectedGenre
+      ? allmovies.filter((m) => m.genre._id === selectedGenre._id)
+      : allmovies;
+
+    const movies = Paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
         <div className="col-3">
           <ListGroup
             items={this.state.genres}
+            textProperty="name"
+            valueProperty="_id"
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
         <div className="col">
-          <p>Showing {count} movies in the database.</p>
+          <p>Showing {filtered.length} movies in the database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -94,7 +106,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             onPageChange={this.handlePageChange}
             currentPage={currentPage}
--- src/components/common/ListGroup.jsx
+++ src/components/common/ListGroup.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 const ListGroup = (props) => {
-  const { items } = props;
+  const { items, textProperty, valueProperty, onItemSelect, selectedItem } =
+    props;
 
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <li key={item._id} className="list-group-item">
-          {item.name}
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
+          {item[textProperty]}
         </li>
       ))}
     </ul>
